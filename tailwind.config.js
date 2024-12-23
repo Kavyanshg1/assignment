d496418edaf2741c9e1190fb4b80ddff001c38e0
Kavyanshg1 <166997759+Kavyanshg1@users.noreply.github.com>
2024-12-23 10:25:55 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust the paths to your components
+  ],
+  darkMode: 'class', // Enable dark mode using the class strategy
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
